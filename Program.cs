e193bc698c952df7647719b21eb0f00fbd61d245
Joseph Gitonga <josephkimanigitonga370@gmail.com>
2024-10-16 17:20:17 +0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿class program
+{
+
+
+static void Main(string[] args)
+    {
+        Console.WriteLine("Type password you want to encrypt");
+
+        string plain=Console.ReadLine();
+
+        string hashedPassword = BCrypt.Net.BCrypt.HashPassword(plain, 13);
+
+        bool isCorrect= BCrypt.Net.BCrypt.Verify(plain, hashedPassword);
+        Console.WriteLine(hashedPassword);
+        Console.WriteLine(isCorrect);
+
+      
+
+
+    }
+
+
+  
+
+
+
+} 
